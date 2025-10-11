f83d356395216177ce61fbd9a0a346ede5febbcb
TheSecretUndergroundCoder <135362041+TheSecretUndergroundCoder@users.noreply.github.com>
2025-10-11 10:08:51 +0100

    Remving unecisary things

--- start-server.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Start a static file server on port 8000 using npx http-server (Node.js required)
-cd /d "%~dp0"
-npx http-server -p 8000 -a localhost -c-1
-pause
--- start-server.ps1
+++
@@ -1,4 +0,0 @@
-# Start a static file server on port 8000 using npx http-server (Node.js required)
-Set-Location -Path $PSScriptRoot
-npx http-server -p 8000 -a localhost -c-1
-Read-Host "Press Enter to exit..."
