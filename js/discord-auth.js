9bd05c1e0d38844475a34c4183d34ac01408f466
TheSecretUndergroundCoder <135362041+TheSecretUndergroundCoder@users.noreply.github.com>
2025-11-01 20:14:02 +0000

    fix AGAIN :)

--- js/discord-auth.js
+++ js/discord-auth.js
@@ -2,7 +2,7 @@
 class DiscordAuth {
     constructor() {
         this.CLIENT_ID = '1340376847732707380';
-        this.REDIRECT_URI = window.location.origin + '/auth-callback.html';
+        this.REDIRECT_URI = window.location.origin + '/index.html';
         this.SCOPES = 'identify guilds.members.read';
         this.GUILD_ID = '1152677388543598749'; 
     }
