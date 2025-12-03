79ea0d88e48ea248310a0b2537d9425c52a7f144
TheSecretUndergroundCoder <135362041+TheSecretUndergroundCoder@users.noreply.github.com>
2025-12-03 21:40:49 +0000

    ghdfgh

--- api/save.php
+++ api/save.php
@@ -2,6 +2,10 @@
 // Temporarily disable rate limiter for testing
 // require_once 'rate-limiter.php';
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, OPTIONS');
